67c5cd7e48d72aea76807e36c9f05943fe4d231c
Dauve318G787 <93968142+Dauve318G787@users.noreply.github.com>
2023-10-14 17:20:46 -0500

    Add files via upload

--- CS325_p3/module_3/getProcessedData.py
+++ CS325_p3/module_3/getProcessedData.py
@@ -7,38 +7,38 @@ import sys #allows us to use command line args
 import praw
 from praw.models import MoreComments
 
-saveDir = "C:\\Users\\12242\\Desktop\\CS325_p3\\Data\\\\processed\\processed.txt"
+saveDir = "C:\\Users\\12242\\Desktop\\CS325_p3\\Data\\\\processed\\processed.txt" #this is where the output text file will go
 
 redditCommentFile = open("websiteData.txt", "w")
 
-reddit_read_only = praw.Reddit(client_id="o4gdzZa2AGpARW1htVDMcg",
+reddit_read_only = praw.Reddit(client_id="o4gdzZa2AGpARW1htVDMcg", #initializes connection to API
                                client_secret="HDSYgES1OHiZL5KjVHj4mgBo7dnjwQ",
                                user_agent="David Dupuis")
 
-subreddit = reddit_read_only.subreddit(sys.argv[1])
+subreddit = reddit_read_only.subreddit(sys.argv[1]) #takes subreddit name via CLI
 
 print("Subreddit Name: ", subreddit.display_name)
 print("Subreddit Description: ", subreddit.description)
 
 url = sys.argv[2]
 
-submission = reddit_read_only.submission(url=url)
+submission = reddit_read_only.submission(url=url) #creates submission object
 
-comments = submission.comments
+comments = submission.comments #loads comments into a variable
 
 def extractComments(comments, level=0):
 
-    for comment in comments:
+    for comment in comments: #for each comment...
 
         commentBody = comment.body
 
-        with open(saveDir, 'a') as commentFile:
+        with open(saveDir, 'a') as commentFile: #write each comment to the output file at the given directory
 
             commentFile.write('Reply: ' * level + commentBody + "\n")
         
-        if comment.replies:
+        if comment.replies: #if there are replies...
 
-            extractComments(comment.replies, level + 1)
+            extractComments(comment.replies, level + 1) #print replies in a hierarchical format
 
 
 extractComments(comments)
